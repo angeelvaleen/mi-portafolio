e5874d66c699205df9526c1a02799be59cadd455
angeelvaleencia <valencialopez046@gmail.com>
2026-07-02 16:03:22 -0600

    Actualizando configuración de variables

--- src/components/sections/ContactoSection.jsx
+++ src/components/sections/ContactoSection.jsx
@@ -29,7 +29,7 @@ export default function ContactoSection() {
     setSending(true);
 
     try {
-      const response = await fetch("/api/send", {
+      const response = await fetch("https://angeelsystem.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
